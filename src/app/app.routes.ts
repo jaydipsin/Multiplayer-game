54e7347ce248af055697c803909af2f0c51aa242
jaydip singh <jaydipsinghchouhan93@gmail.com>
2025-09-28 21:43:57 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth/auth').then((m) => m.Auth),
+  },
+];
