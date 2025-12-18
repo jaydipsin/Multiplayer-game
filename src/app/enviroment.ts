5b2b8a334673ccec3a347033d0b674682f3508bc
jaydip singh <jaydipsinghchouhan93@gmail.com>
2025-12-18 23:25:23 +0530

    Stagging

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,4 +1,4 @@
 // The local url
 
-export const URL = "http://localhost:8000"
-// export const URL = "https://multiplayer-game-be-t5pn.onrender.com"
+// export const URL = "http://localhost:8000"
+export const URL = "https://multiplayer-game-be-t5pn.onrender.com"
